8673520eee9231f137a7671644f826c6f5eb28a8
YoureaBosonHarry <rath3.14159@protonmail.com>
2020-07-21 00:33:10 -0500

    Fixed flask return error

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import jsonify, Flask, request
+from Lantern_Controller import Lantern_Manager
 
 app = Flask(__name__)
+lantern_manager = Lantern_Manager()
 
 @app.route('sendPattern', methods=['POST'])
 def sendPattern():
-    pass
-
+    lantern_id = request.args.get("id")
+    pattern = request.args.get("pattern")
+    hex_color = request.args.get("hexColor")
+    frequency = request.args.get("frequency")
+    lantern_manager.change_pattern(lantern_id, pattern, hex_color, frequency)
+    return jsonify(status=200)
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8000)
